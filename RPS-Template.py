0de9a120e994ed555e1b209e10231105465a740a
Rose Hammond <rosehammond@hotmail.co.uk>
2023-09-06 17:38:34 +0100

    Updates RPS-Template.py with useful comments. Creates manual_rps.py which has all the code to run the game of rock paper scissors manually.

--- RPS-Template.py
+++ RPS-Template.py
@@ -6,21 +6,22 @@ cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 
-#The main thing to understand is that the variable predictions contains the output of the model, 
+# The main thing to understand is that the variable predictions contains the output of the model, 
 # and each element in the output corresponds to the probability of the input image representing 
 # a particular class.
-#So, for example, if the prediction has the following output: [[0.8, 0.1, 0.05, 0.05]], 
+# So, for example, if the prediction has the following output: [[0.8, 0.1, 0.05, 0.05]], 
 # there is an 80% chance that the input image shows rock, a 10% chance that it shows paper, 
 # a 5% chance that it shows scissors, and a 5% chance that it shows nothing.
 
-#Notice that the prediction is a numpy array with one row and four columns. 
+# Notice that the prediction is a numpy array with one row and four columns. 
 # So first, you need to access the first row, and then get the index of the highest value in the row. 
-# Check this link  to see how to get the index of the highest value in a row.
+# Check this link https://numpy.org/doc/stable/reference/generated/numpy.argmax.html to see how to get 
+# the index of the highest value in a row.
 
-#Before moving on, you need to make sure you understand how Python works. 
+# Before moving on, you need to make sure you understand how Python works. 
 # So make sure you've understood the prerequisite content.
 
-#You have to know at least how to effectively use while loops and if statements. 
+# You have to know at least how to effectively use while loops and if statements. 
 # Please, don't move on until you have a solid understanding of these concepts.
 
 while True: 
